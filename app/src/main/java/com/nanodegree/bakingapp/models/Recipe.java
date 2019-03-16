e2f55714e060a48ef91e9fde515a10c5bc8099af
veli <velly_mathebula@rocketmail.com>
2019-03-16 21:54:33 +0200

    Added Models

---
+++ app/src/main/java/com/nanodegree/bakingapp/models/Recipe.java
@@ -0,0 +1,116 @@
+package com.nanodegree.bakingapp.models;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class Recipe implements Parcelable {
+    @SerializedName("id")
+    @Expose
+    private long id;
+
+    @SerializedName("name")
+    @Expose
+    private String name;
+
+    @SerializedName("ingredients")
+    @Expose
+    private List<Ingredient> ingredients;
+
+    @SerializedName("steps")
+    @Expose
+    private List<Step> steps;
+
+    @SerializedName("servings")
+    @Expose
+    private long servings;
+
+    @SerializedName("image")
+    @Expose
+    private String image;
+
+
+    protected Recipe(Parcel in) {
+        id = in.readLong();
+        name = in.readString();
+        servings = in.readLong();
+        image = in.readString();
+    }
+
+    public static final Creator<Recipe> CREATOR = new Creator<Recipe>() {
+        @Override
+        public Recipe createFromParcel(Parcel in) {
+            return new Recipe(in);
+        }
+
+        @Override
+        public Recipe[] newArray(int size) {
+            return new Recipe[size];
+        }
+    };
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(List<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public List<Step> getSteps() {
+        return steps;
+    }
+
+    public void setSteps(List<Step> steps) {
+        this.steps = steps;
+    }
+
+    public long getServings() {
+        return servings;
+    }
+
+    public void setServings(long servings) {
+        this.servings = servings;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeLong(id);
+        dest.writeString(name);
+        dest.writeLong(servings);
+        dest.writeString(image);
+    }
+}
