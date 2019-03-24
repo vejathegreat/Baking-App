86597f70912d5060fabfa8493e0009b755f6fc9c
veli <velly_mathebula@rocketmail.com>
2019-03-24 23:54:46 +0200

    added ui tests and uncommitted files

--- app/src/androidTest/java/com/nanodegree/bakingapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/nanodegree/bakingapp/ExampleInstrumentedTest.java
@@ -1,13 +1,23 @@
 package com.nanodegree.bakingapp;
 
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
 
+import com.nanodegree.bakingapp.features.landing.MainActivity;
+
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.test.espresso.contrib.RecyclerViewActions;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -16,11 +26,41 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.nanodegree.bakingapp", appContext.getPackageName());
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void testThatRecipeListAreDisplayedToRecycler() throws InterruptedException {
+        Thread.sleep(2000);
+        if (getRecipeRecyclerViewItemCount() > 0) {
+            onView(withId(R.id.recipe_list_recyclerView)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+        }
     }
+
+    @Test
+    public void testThatRecipeDetailsAreDisplayedWhenItemSelected() throws InterruptedException {
+        Thread.sleep(2000);
+        if (getRecipeRecyclerViewItemCount() > 0) {
+            onView(withId(R.id.recipe_list_recyclerView)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+        }
+        onView(withId(R.id.activity_step_list_layout)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void testStepListItemsAreDisplayedWhenStepClicked() throws InterruptedException {
+        Thread.sleep(2000);
+        if (getRecipeRecyclerViewItemCount() > 0) {
+            onView(withId(R.id.recipe_list_recyclerView)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+        }
+        Thread.sleep(4000);
+        onView(withId(R.id.activity_step_list_layout)).check(matches(isDisplayed()));
+    }
+
+    private int getRecipeRecyclerViewItemCount() {
+        RecyclerView recyclerView = mActivityRule.getActivity().findViewById(R.id.recipe_list_recyclerView);
+        return recyclerView.getAdapter().getItemCount();
+    }
+
 }
